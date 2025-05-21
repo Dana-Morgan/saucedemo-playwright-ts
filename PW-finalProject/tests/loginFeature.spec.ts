93653d952f60c373e4bf4fb976f138ba272f9827
Dana-Morgan <151971253+Dana-Morgan@users.noreply.github.com>
2025-05-22 00:47:54 +0300

    first commit

---
+++ PW-finalProject/tests/loginFeature.spec.ts
@@ -0,0 +1,27 @@
+import {test, expect} from '@playwright/test';
+
+test('Add product to cart and check contents then logout', async({page}) => {
+  await page.goto('https://www.saucedemo.com/')
+
+  await page.locator('[data-test="username"]').fill('standard_user')
+  await page.locator('[data-test="password"]').fill('secret_sauce')
+  await page.locator('[data-test="login-button"]').click()
+
+  await expect(page).toHaveURL(/.*inventory.html/)
+
+  await page.locator('[data-test="add-to-cart-sauce-labs-onesie"]').click()
+
+  await page.locator('.shopping_cart_link').click()
+
+  await expect(page).toHaveURL(/.cart.html/)
+
+  const cartItem = page.locator('.inventory_item_name')
+
+  await expect(cartItem).toContainText('Sauce Labs Onesie')
+
+  await page.locator('#react-burger-menu-btn').click()
+  
+  await page.locator('#logout_sidebar_link').click()
+  
+  await expect(page).toHaveURL('https://www.saucedemo.com/')
+})
